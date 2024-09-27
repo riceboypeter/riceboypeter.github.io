70311a6c909c7cc848a2515a78b3046675bfcc16
Peter Qiu <77300475+riceboypeter@users.noreply.github.com>
2024-09-27 15:14:03 -0700

    still just some setup stuff \(￣︶￣*\))

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
+// cursor stuff
 // find the cursor element
-const cursor = document.querySelector('.custom-cursor');
+const cursor = document.querySelector('#custom-cursor');
 
 // tracks the element to the actual mouse position
 const positionElement = (e)=> {
-  const mouseY = e.clientY;
-  const mouseX = e.clientX;
-   
-  cursor.style.left = `${mouseX}px`;
-  cursor.style.top = `${mouseY}px`;
+    const mouseY = e.clientY;
+    const mouseX = e.clientX;
+
+    cursor.style.left = `${mouseX}px`;
+    cursor.style.top = `${mouseY}px`;
 }
 
 // look for when it moves
 window.addEventListener('mousemove', positionElement);
+
+// navbar stuff
+// when the user scrolls, collapse the navbar
+var prevScrollpos = window.scrollY;
+const navbar = document.getElementById("navbar");
+window.onscroll = function() {
+    var currentScrollPos = window.scrollY;
+    if (prevScrollpos > currentScrollPos) {
+        navbar.style.top = "0";
+    } else {
+        navbar.style.top = "-65px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+
+// Detect scrolling and hide the arrow
+const downArrow = document.getElementById('down-arrow');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) { // Hide the arrow when you scroll more than 50px
+    downArrow.style.opacity = '0';
+  } else {
+    downArrow.style.opacity = '1';
+  }
+});
