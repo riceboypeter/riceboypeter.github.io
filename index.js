b70b9a8f36bf7b9a4406561f9010204022fc1a88
Peter Qiu <77300475+riceboypeter@users.noreply.github.com>
2024-09-27 16:49:25 -0700

    looked at some portfolios.. idk where to go from here

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 // cursor stuff
 // find the cursor element
 const cursor = document.querySelector('#custom-cursor');
-
+cursor.style.opacity = '0';
 // tracks the element to the actual mouse position
 const positionElement = (e)=> {
     const mouseY = e.clientY;
     const mouseX = e.clientX;
-
     cursor.style.left = `${mouseX}px`;
     cursor.style.top = `${mouseY}px`;
+    cursor.style.opacity = '1';
 }
-
 // look for when it moves
 window.addEventListener('mousemove', positionElement);
 
 // navbar stuff
-// when the user scrolls, collapse the navbar
+// when the user scrolls down, collapse the navbar
 var prevScrollpos = window.scrollY;
 const navbar = document.getElementById("navbar");
 window.onscroll = function() {
@@ -28,14 +27,21 @@ window.onscroll = function() {
     prevScrollpos = currentScrollPos;
 }
 
-
-// Detect scrolling and hide the arrow
+// down arrow stuff
+// get down arrow element
 const downArrow = document.getElementById('down-arrow');
-
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 50) { // Hide the arrow when you scroll more than 50px
-    downArrow.style.opacity = '0';
-  } else {
-    downArrow.style.opacity = '1';
-  }
+    if (window.scrollY > 50) { // when the user scrolls more than 50
+        downArrow.style.opacity = '0'; // hide that bitch
+    } else {
+        downArrow.style.opacity = '1';
+    }
 });
+
+window.onload = function() {
+    // Add the slide-in class to the navbar
+    navbar.classList.add('slide-in');
+    // Add fade-in class to the down arrow
+    downArrow.style.opacity = '1';
+    
+};
